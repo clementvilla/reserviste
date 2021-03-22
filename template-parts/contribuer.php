a0ea74940d44421a59f9bd09594165064de3d104
Khallan <a.pinot986@laposte.net>
2021-03-22 15:01:57 +0100

    Correction de la mise à jour des liens du theme
    Correction de quelques liens du nettoyage précédent

--- template-parts/contribuer.php
+++ template-parts/contribuer.php
@@ -8,7 +8,7 @@ Template Name: pagecontribuer
 get_header();
 ?>
 
-<div  class="sengager" style=background-image:url(<?php echo get_bloginfo('template_directory')?>."/images/fond-engager.jpg");>
+<div  class="sengager" style=background-image:url(<?php echo get_bloginfo('template_directory')?>/images/fond-engager.jpg);>
     <section class="slogan">
         <h1>CONTRIBUER</h1>
         <img alt="logo header" src=<?php echo get_bloginfo('template_directory')?>/images/images-galerie/logo-titre.png>
--- template-parts/home.php
+++ template-parts/home.php
@@ -12,7 +12,7 @@ get_header();
     <section class="slogan">
         <h1>ASSOCIATION DES RÉSERVISTES</h1>
         <h3>DU PAYS DE MONTBÉLIARD</h3>
-        <img src=<?php echo get_bloginfo('template_directory')?>"/images/logo-daccueil.png"  alt="logo du site"/>
+        <img src="<?php echo get_bloginfo('template_directory')?>/images/logo-daccueil.png"  alt="logo du site"/>
         <p>S'engager pour la nation, dans chacune de nos vies</p>
     </section>
 </div>
@@ -30,15 +30,15 @@ get_header();
     <div class="carousel-inner">
         <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
         <div class="carousel-item">
-            <img src=<?php echo get_bloginfo('template_directory')?>"/images/drapeau-association.jpg"/>
+            <img src="<?php echo get_bloginfo('template_directory')?>/images/drapeau-association.jpg">
         </div>
         <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
         <div class="carousel-item">
-            <img src=<?php echo get_bloginfo('template_directory')?>"/images/membre.jpg"/>
+            <img src="<?php echo get_bloginfo('template_directory')?>/images/membre.jpg">
         </div>
         <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
         <div class="carousel-item">
-            <img src=<?php echo get_bloginfo('template_directory')?>"/images/images-galerie/lecture.jpg"/>
+            <img src="<?php echo get_bloginfo('template_directory')?>/images/images-galerie/lecture.jpg">
         </div>
         <label for="carousel-3" class="carousel-control prev control-1">‹</label>
         <label for="carousel-2" class="carousel-control next control-1">›</label>
