826004bd3cbe149a0d65c7b0ffa7182fbd2cab42
Villa <clement.villa@edu.univ-fcomte.fr>
2021-04-01 10:39:39 +0200

    suppression des images inutiles

--- header.php
+++ header.php
@@ -33,91 +33,12 @@
         <div class="header">
 
             <?php
-
             wp_nav_menu(array(
                 'theme_location' => 'menu-1'
             )); ?>
         </div>
 
 
-        <!--       <div class="menu-content">
-                   <section class="menu">
-                       <label class="open-menu-all" for="open-menu-all">
-                           <input class="input-menu-all" id="open-menu-all" type="checkbox" name="menu-open"/>
-                           <div class="navegacao-mobile">
-                               <span class="linha-menu"></span>
-                               <span class="linha-menu"></span>
-                               <span class="linha-menu"></span>
-                           </div>
-
-                           <ul class="list-menu">
-
-
-
-                               <li class="item-menu">
-                                   <a href="#" class="link-menu">Vie Associative</a>
-                                   <ul class="sous">
-                                       <li>
-                                           <a href=?php echo home_url() . "/contribuer-2/" ?>  class="link-menu">Contribuer</a>
-                                       </li>
-                                       <li><a href=?php echo home_url() . "/histoire/" ?> class="link-menu">Histoire</a></li>
-                                       <li><a href=<php echo home_url() . "/internationnal/" ?> class="link-menu">L'internationnal</a>
-                                       </li>
-                                   </ul>
-                               </li>
-
-                               <li class="item-menu">
-                                   <a href="#" target="_blank" class="link-menu">Actualités</a>
-                                   <ul class="sous">
-                                       <li><a href=?php echo home_url() . "/article-type/" ?>  class="link-menu">Articles</a>
-                                       </li>
-                                       <li><a href=?php echo home_url() . "/calendrier/" ?>  class="link-menu">Évenements</a>
-                                       </li>
-                                   </ul>
-                               </li>
-                               <li class="item-menu">
-                                   <a href=?php echo home_url() . "/sengager/" ?>  class="link-menu">S'engager</a>
-                               </li>
-                               <li class="item-menu">
-                                   <a href="#" target="_blank" class="link-menu">Activités</a>
-                                   <ul class="sous">
-                                       <li><a href="#">Bulletins</a></li>
-                                       <li><a href=?php echo home_url() . "/pageDessin/" ?>  class="link-menu">Dessins de
-                                               Noël</a></li>
-                                       <li><a href="#">Conférences</a></li>
-                                   </ul>
-                               </li>
-
-                               <li class="item-menu">
-                                   <a href="#" target="_blank" class="link-menu">Découvrir la garde</a>
-                                   <ul class="sous">
-                                       <li><a href=?php echo home_url() . "/reserve-citoyenne/" ?>  class="link-menu">Garde
-                                               citoyenne</a></li>
-                                       <li><a href=?php echo home_url() . "/garde-operationnelle/" ?>  class="link-menu">Garde
-                                               opérationnelle</a></li>
-                                   </ul>
-                               </li>
-
-                               <li class="item-menu">
-                                   <a href="https://reservistesdupaysdemontbeliard.cbouvier.fr/galerie-2/" class="link-menu">Galerie</a>
-                               </li>
-
-                               <a href=?php echo home_url() . "/wp-admin" ?>>
-
-                                   <figure>
-                                       <img alt="boutton de connexion"
-                                            src= ?php echo home_url() . "/wp-content/uploads/2018/12/user-white.png" ?>>
-
-                                   </figure>
-                               </a>
-
-                           </ul>
-
-                       </label>
-
-
-                   </section>
-               </div>-->
 
 
     </header>
